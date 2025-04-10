71af8cbb054cc20ca186aa1492a9c5b62ec0698c
argha-saha <saha56@purdue.edu>
2025-04-10 19:47:50 -0400

    Changed how mnemonic and operands are extracted from the line

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -1,5 +1,5 @@
 use crate::assembler::error::AssemblerError;
-use crate::assembler::instructions::{InstructionSet, InstructionType};
+use crate::assembler::instructions::{InstructionFormat, InstructionSet, InstructionType};
 
 struct Parser {
     instructions: InstructionSet
@@ -23,19 +23,54 @@ impl Parser {
             return Ok(None);
         }
 
+        // "add x4, x5, x6" -> vec!["add", "x4", "x5", "x6"]
         let parts: Vec<&str> = line.split_whitespace().collect();
-        let (mnemonic, operands) = parts.split_first().unwrap();
+        let mnemonic = parts[0];
+        let operands = &parts[1..];
+
+        if operands.is_empty() {
+            return Err(AssemblerError::ParseError("Missing operands".into()));
+        }
 
         let instr = self.instructions.get_instruction(mnemonic)
             .ok_or_else(|| AssemblerError::InvalidInstruction(mnemonic.to_string()))?.clone();
 
         match instr.fmt {
             InstructionType::R => self.parse_r_type(&instr, operands),
-            _ => Err(AssemblerError::ParseError("Invalid/unsupported Instruction FMT".into())),
+            _ => Err(AssemblerError::ParseError("Invalid/unsupported instruction FMT".into())),
         }
     }
 
-    // TODO: Parse R-type instructions
+    // Parse R-type instructions
+    fn parse_r_type(&self, fmt: &InstructionFormat, operands: &[&str]) -> Result<u32, AssemblerError> {
+        if operands.len() != 3 {
+            return Err(AssemblerError::ParseError(format!(
+                "Expected 3 operands but received {}",
+                operands.len()
+            )));
+        }
+
+        let rd = parse_register(operands[0])?;
+        let rs1 = parse_register(operands[1])?;
+        let rs2 = parse_register(operands[2])?;
+
+        Ok(encode_r_type(
+            fmt.opcode,
+            rd,
+            fmt.funct3.unwrap(),
+            rs1,
+            rs2,
+            fmt.funct7.unwrap()
+        ))
+    }
 
     // TODO: Parse I-type instructions
+}
+
+fn parse_register(p0: &str) -> _ {
+    todo!()
+}
+
+fn encode_r_type(p0: u32, p1: _, p2: u32, p3: _, p4: _, p5: u32) -> u32 {
+    todo!()
 }
