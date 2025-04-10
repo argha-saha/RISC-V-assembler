4d98ef229c474a60d59b9919bf6ebfdf912888e8
Argha Saha <arghasaha@icloud.com>
2025-04-10 02:09:01 -0400

    Added check for assembly file CLI argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error::Error;
+use std::process::exit;
+
+mod assembler;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    // Check for assembly file
+    if args.len() != 2 {
+        eprintln!("Usage: {} <asm_file>", args[0]);
+        exit(1);
+    }
+
+    Ok(())
 }
