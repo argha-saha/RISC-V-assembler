23ae1aa404091ab229f44e1a41a949cbc1a1036a
Argha Saha <arghasaha@icloud.com>
2025-04-10 03:40:19 -0400

    Implemented fmt display for assembler-related errors for clearer error messages

--- src/assembler/error.rs
+++ src/assembler/error.rs
@@ -1,7 +1,21 @@
+use std::fmt;
+use std::io::Error;
+
 #[derive(Debug)]
 pub enum AssemblerError {
-    IOError(std::io::Error),
+    IOError(Error),
     ParserError(String),
     InvalidInstruction(String),
     InvalidOperand(String)
+}
+
+impl fmt::Display for AssemblerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::IOError(e) => write!(f, "IO Error: {}", e),
+            Self::ParserError(e) => write!(f, "Parser Error: {}", e),
+            Self::InvalidInstruction(e) => write!(f, "Invalid Instruction: {}", e),
+            Self::InvalidOperand(e) => write!(f, "Invalid Operand: {}", e)
+        }
+    }
 }
