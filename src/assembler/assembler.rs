c2bb710aae8dc46991434dba73f5a937cc11625e
argha-saha <saha56@purdue.edu>
2025-04-12 18:18:24 -0400

    Wrote the parsing functions for each type of instruction

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -39,7 +39,11 @@ impl Parser {
 
         match instr.fmt {
             InstructionType::R => Ok(Some(self.parse_r_type(&instr, operands)?)),
-            _ => Err(AssemblerError::ParseError("Invalid/unsupported instruction FMT".into())),
+            InstructionType::I => Ok(Some(self.parse_i_type(&instr, operands)?)),
+            InstructionType::S => Ok(Some(self.parse_s_type(&instr, operands)?)),
+            InstructionType::B => Ok(Some(self.parse_b_type(&instr, operands)?)),
+            InstructionType::U => Ok(Some(self.parse_u_type(&instr, operands)?)),
+            InstructionType::J => Ok(Some(self.parse_j_type(&instr, operands)?))
         }
     }
 
@@ -47,7 +51,7 @@ impl Parser {
     fn parse_r_type(&self, fmt: &InstructionFormat, operands: &[&str]) -> Result<u32, AssemblerError> {
         if operands.len() != 3 {
             return Err(AssemblerError::ParseError(format!(
-                "Expected 3 operands but received {}",
+                "Expected 3 operands but received {} for an r-type instruction",
                 operands.len()
             )));
         }
@@ -66,7 +70,115 @@ impl Parser {
         ))
     }
 
-    // TODO: Parse I-type instructions
+    fn parse_i_type(&self, fmt: &InstructionFormat, operands: &[&str]) -> Result<u32, AssemblerError> {
+        let (rd, rs1, imm) = match operands.len() {
+            // I-type load instructions
+            2 => {
+                let rd = parse_register(operands[0])?;
+                let (imm, rs1) = parse_offset(operands[1])?;
+                (rd, rs1, imm)
+            }
+
+            3 => {
+                let rd = parse_register(operands[0])?;
+                let rs1 = parse_register(operands[1])?;
+                let imm = parse_immediate(operands[2])?;
+                (rd, rs1, imm)
+            }
+
+            _ => {
+                return Err(AssemblerError::ParseError(format!(
+                    "Expected 2 or 3 operands but received {} for an i-type instruction",
+                    operands.len()
+                )));
+            }
+        };
+
+        Ok(encode_i_type(
+            fmt.opcode,
+            rd,
+            fmt.funct3.unwrap_or(0),
+            rs1,
+            imm
+        ))
+    }
+
+    fn parse_s_type(&self, fmt: &InstructionFormat, operands: &[&str]) -> Result<u32, AssemblerError> {
+        if operands.len() != 2 {
+            return Err(AssemblerError::ParseError(format!(
+                "Expected 2 operands but received {} for an s-type instruction",
+                operands.len()
+            )));
+        }
+
+        let rs2 = parse_register(operands[0])?;
+        let (imm, rs1) = parse_offset(operands[1])?;
+
+        Ok(encode_s_type(
+            fmt.opcode, 
+            fmt.funct3.unwrap_or(0), 
+            rs1, 
+            rs2, 
+            imm
+        ))
+    }
+
+    fn parse_b_type(&self, fmt: &InstructionFormat, operands: &[&str]) -> Result<u32, AssemblerError> {
+        if operands.len() != 3 {
+            return Err(AssemblerError::ParseError(format!(
+                "Expected 3 operands but received {} for a b-type instruction",
+                operands.len()
+            )));
+        }
+
+        let rs1 = parse_register(operands[0])?;
+        let rs2 = parse_register(operands[1])?;
+        let imm = parse_immediate(operands[2])?;
+
+        Ok(encode_b_type(
+            fmt.opcode,
+            fmt.funct3.unwrap_or(0),
+            rs1,
+            rs2,
+            imm
+        ))
+    }
+
+    fn parse_u_type(&self, fmt: &InstructionFormat, operands: &[&str]) -> Result<u32, AssemblerError> {
+        if operands.len() != 2 {
+            return Err(AssemblerError::ParseError(format!(
+                "Expected 2 operands but received {} for a u-type instruction",
+                operands.len()
+            )));
+        }
+
+        let rd = parse_register(operands[0])?;
+        let imm = parse_immediate(operands[1])?;
+
+        Ok(encode_u_type(
+            fmt.opcode,
+            rd,
+            imm
+        ))
+    }
+
+    fn parse_j_type(&self, fmt: &InstructionFormat, operands: &[&str]) -> Result<u32, AssemblerError> {
+        if operands.len() != 2 {
+            return Err(AssemblerError::ParseError(format!(
+                "Expected 2 operands but received {} a j-type instruction",
+                operands.len()
+            )));
+        }
+
+        let rd = parse_register(operands[0])?;
+        let imm = parse_immediate(operands[1])?;
+
+        Ok(encode_j_type(
+            fmt.opcode,
+            rd,
+            imm
+        ))
+    }
 }
 
 const ABI_NAMES: phf::Map<&'static str, u32> = phf::phf_map! {
@@ -115,9 +227,9 @@ pub fn parse_register(register: &str) -> Result<u32, AssemblerError> {
     }
 
     // Check if the register is x0 to x31 or $0 to $31
-    if let Some(stripped) = reg_name.strip_prefix('x') {
+    if let Some(stripped) = reg_name.strip_prefix('x') {         // RISC-V style
         reg_name = stripped.to_string();
-    } else if let Some(stripped) = reg_name.strip_prefix('$') {
+    } else if let Some(stripped) = reg_name.strip_prefix('$') {  // MIPS style
         reg_name = stripped.to_string();
     }
 
