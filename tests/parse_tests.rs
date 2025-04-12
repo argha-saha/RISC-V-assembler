8bb9206e66bd7ddf9be6257e6a4254bf17294465
argha-saha <saha56@purdue.edu>
2025-04-12 03:26:09 -0400

    Wrote test for MIPS style assembly and 1 ABI name test

--- tests/parse_tests.rs
+++ tests/parse_tests.rs
@@ -3,9 +3,23 @@ mod tests {
     use riscv_assembler::assembler::assembler::parse_register;
 
     #[test]
-    fn test_abi_names() {
+    fn test_regular_names() {
         let reg_name = "x4";
         let reg_num = parse_register(reg_name).unwrap();
         assert_eq!(reg_num, 4);
+    }
+
+    #[test]
+    fn test_mips_style_names() {
+        let reg_name = "$4";
+        let reg_num = parse_register(reg_name).unwrap();
+        assert_eq!(reg_num, 4);
+    }
+
+    #[test]
+    fn test_abi_names() {
+        let reg_name = "a0";
+        let reg_num = parse_register(reg_name).unwrap();
+        assert_eq!(reg_num, 10);
     }
 }
