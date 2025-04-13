739ab550d1b951370c03c1b13729d10e594a344c
Argha Saha <arghasaha@icloud.com>
2025-04-13 01:53:18 -0400

    Wrote a struct for the assembler and the collect_labels function to store labels in a hashmap

--- src/assembler/error.rs
+++ src/assembler/error.rs
@@ -1,7 +1,8 @@
-use std::fmt;
+use std::{fmt, io};
 
 #[derive(Debug, PartialEq)]
 pub enum AssemblerError {
+    IOError(String),
     ParseError(String),
     InvalidInstruction(String),
     InvalidOperand(String)
@@ -10,9 +11,18 @@ pub enum AssemblerError {
 impl fmt::Display for AssemblerError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Self::IOError(e) => write!(f, "IO Error: {}", e),
             Self::ParseError(e) => write!(f, "Parser Error: {}", e),
             Self::InvalidInstruction(e) => write!(f, "Invalid Instruction: {}", e),
             Self::InvalidOperand(e) => write!(f, "Invalid Operand: {}", e)
         }
+    }
+}
+
+impl std::error::Error for AssemblerError {}
+
+impl From<io::Error> for AssemblerError {
+    fn from(e: io::Error) -> Self {
+        Self::IOError(e.to_string())
     }
 }
