cd876fce80453a2b11556a346abacc6d41e32ac5
argha-saha <saha56@purdue.edu>
2025-04-14 04:16:44 -0400

    Replaced instruction hashmap with phf map for improved performance

--- src/assembler/instructions.rs
+++ src/assembler/instructions.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use phf::phf_map;
 
 #[derive(Debug, Copy, Clone)]
 pub enum InstructionType {
@@ -18,346 +18,344 @@ pub struct InstructionFormat {
     pub funct7: Option<u32>
 }
 
-pub struct InstructionSet {
-    instructions: HashMap<&'static str, InstructionFormat>
-}
+static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
+    /* R-type Instructions */
+
+    // ADD
+    "add" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b000),
+        funct7: Some(0b0000000)
+    },
+
+    // SUB
+    "sub" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b000),
+        funct7: Some(0b0100000)
+    },
+
+    // XOR
+    "xor" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b100),
+        funct7: Some(0b0000000)
+    },
+
+    // OR
+    "or" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b110),
+        funct7: Some(0b0000000)
+    },
+
+    // AND
+    "and" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b111),
+        funct7: Some(0b0000000)
+    },
+
+    // Shift Left Logical
+    "sll" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b001),
+        funct7: Some(0b0000000)
+    },
+
+    // Shift Right Logical
+    "srl" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b101),
+        funct7: Some(0b0000000)
+    },
+
+    // Shift Right Arithmetic
+    "sra" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b101),
+        funct7: Some(0b0100000)
+    },
+
+    // Set Less Than
+    "slt" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b010),
+        funct7: Some(0b0000000)
+    },
+
+    // Set Less Than Unsigned
+    "sltu" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0110011,
+        funct3: Some(0b011),
+        funct7: Some(0b0000000)
+    },
+
+    /* I-type Instructions */
+
+    // ADD Immediate
+    "addi" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b000),
+        funct7: None
+    },
+
+    // XOR Immediate
+    "xori" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b100),
+        funct7: None
+    },
+
+    // OR Immediate
+    "ori" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b110),
+        funct7: None
+    },
+
+    // AND Immediate
+    "andi" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b111),
+        funct7: None
+    },
+
+    // Shift Left Logical Immediate
+    "slli" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b001),
+        funct7: Some(0b0000000)
+    },
+
+    // Shift Right Logical Immediate
+    "srli" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b101),
+        funct7: Some(0b0000000)
+    },
+
+    // Shift Right Arithmetic Immediate
+    "srai" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b101),
+        funct7: Some(0b0100000)
+    },
+
+    // Set Less Than Immediate
+    "slti" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b010),
+        funct7: None
+    },
+
+    // Set Less Than Immediate Unsigned
+    "sltiu" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b011),
+        funct7: None
+    },
+
+    /* I-type Load Instructions */
+
+    // Load Byte
+    "lb" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0000011,
+        funct3: Some(0b000),
+        funct7: None
+    },
+
+    // Load Halfword
+    "lh" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0000011,
+        funct3: Some(0b001),
+        funct7: None
+    },
+
+    // Load Word
+    "lw" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0000011,
+        funct3: Some(0b010),
+        funct7: None
+    },
+
+    // Load Byte Unsigned
+    "lbu" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0000011,
+        funct3: Some(0b100),
+        funct7: None
+    },
+
+    // Load Halfword Unsigned
+    "lhu" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0000011,
+        funct3: Some(0b101),
+        funct7: None
+    },
+
+    /* S-type Instructions */
+
+    // Store Byte
+    "sb" => InstructionFormat {
+        fmt: InstructionType::S,
+        opcode: 0b0100011,
+        funct3: Some(0b000),
+        funct7: None
+    },
+
+    // Store Halfword
+    "sh" => InstructionFormat {
+        fmt: InstructionType::S,
+        opcode: 0b0100011,
+        funct3: Some(0b001),
+        funct7: None
+    },
+
+    // Store Word
+    "sw" => InstructionFormat {
+        fmt: InstructionType::S,
+        opcode: 0b0100011,
+        funct3: Some(0b010),
+        funct7: None
+    },
+
+    /* B-type Instructions */
+
+    // Branch ==
+    "beq" => InstructionFormat {
+        fmt: InstructionType::B,
+        opcode: 0b1100011,
+        funct3: Some(0b000),
+        funct7: None
+    },
+
+    // Branch !=
+    "bne" => InstructionFormat {
+        fmt: InstructionType::B,
+        opcode: 0b1100011,
+        funct3: Some(0b001),
+        funct7: None
+    },
+
+    // Branch <
+    "blt" => InstructionFormat {
+        fmt: InstructionType::B,
+        opcode: 0b1100011,
+        funct3: Some(0b100),
+        funct7: None
+    },
+
+    // Branch >=
+    "bge" => InstructionFormat {
+        fmt: InstructionType::B,
+        opcode: 0b1100011,
+        funct3: Some(0b101),
+        funct7: None
+    },
+
+    // Branch < Unsigned
+    "bltu" => InstructionFormat {
+        fmt: InstructionType::B,
+        opcode: 0b1100011,
+        funct3: Some(0b110),
+        funct7: None
+    },
+
+    // Branch >= Unsigned
+    "bgeu" => InstructionFormat {
+        fmt: InstructionType::B,
+        opcode: 0b1100011,
+        funct3: Some(0b111),
+        funct7: None
+    },
+
+    /* U-type Instructions */
+
+    // Load Upper Immediate
+    "lui" => InstructionFormat {
+        fmt: InstructionType::U,
+        opcode: 0b0110111,
+        funct3: None,
+        funct7: None
+    },
+
+    // Add Upper Immediate to PC
+    "auipc" => InstructionFormat {
+        fmt: InstructionType::U,
+        opcode: 0b0010111,
+        funct3: None,
+        funct7: None
+    },
+
+    /* Jump Instructions */
+
+    // Jump and Link
+    "jal" => InstructionFormat {
+        fmt: InstructionType::J,
+        opcode: 0b1101111,
+        funct3: None,
+        funct7: None
+    },
+
+    // Jump and Link Register
+    "jalr" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b1100111,
+        funct3: None,
+        funct7: None
+    },
+
+    /* System Instructions */
+
+    // Environment Call
+    "ecall" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b1110011,
+        funct3: Some(0b000),
+        funct7: Some(0b0000000)
+    },
+
+    // Environment Break
+    "ebreak" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b1110011,
+        funct3: Some(0b000),
+        funct7: Some(0b0000001)
+    },
+};
+
+pub struct InstructionSet;
 
 impl InstructionSet {
     pub fn new() -> Self {
-        let mut instructions = HashMap::new();
-
-        // R-type Instructions
-
-        // ADD
-        instructions.insert("add", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b000),
-            funct7: Some(0b0000000)
-        });
-
-        // SUB
-        instructions.insert("sub", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b000),
-            funct7: Some(0b0100000)
-        });
-
-        // XOR
-        instructions.insert("xor", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b100),
-            funct7: Some(0b0000000)
-        });
-
-        // OR
-        instructions.insert("or", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b110),
-            funct7: Some(0b0000000)
-        });
-
-        // AND
-        instructions.insert("and", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b111),
-            funct7: Some(0b0000000)
-        });
-
-        // Shift Left Logical
-        instructions.insert("sll", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b001),
-            funct7: Some(0b0000000)
-        });
-
-        // Shift Right Logical
-        instructions.insert("srl", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b101),
-            funct7: Some(0b0000000)
-        });
-
-        // Shift Right Arithmetic
-        instructions.insert("sra", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b101),
-            funct7: Some(0b0100000)
-        });
-
-        // Set Less Than
-        instructions.insert("slt", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b010),
-            funct7: Some(0b0000000)
-        });
-
-        // Set Less Than (Unsigned)
-        instructions.insert("sltu", InstructionFormat {
-            fmt: InstructionType::R,
-            opcode: 0b0110011,
-            funct3: Some(0b011),
-            funct7: Some(0b0000000)
-        });
-
-        // I-type Instructions
-
-        // ADD Immediate
-        instructions.insert("addi", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b000),
-            funct7: None
-        });
-
-        // XOR Immediate
-        instructions.insert("xori", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b100),
-            funct7: None
-        });
-
-        // OR Immediate
-        instructions.insert("ori", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b110),
-            funct7: None
-        });
-
-        // AND Immediate
-        instructions.insert("andi", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b111),
-            funct7: None
-        });
-
-        // Shift Left Logical Immediate
-        instructions.insert("slli", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b001),
-            funct7: Some(0b0000000)
-        });
-
-        // Shift Right Logical Immediate
-        instructions.insert("srli", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b101),
-            funct7: Some(0b0000000)
-        });
-
-        // Shift Right Arithmetic Immediate
-        instructions.insert("srai", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b101),
-            funct7: Some(0b0100000)
-        });
-
-        // Set Less Than Immediate
-        instructions.insert("slti", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b010),
-            funct7: None
-        });
-
-        // Set Less Than Immediate (Unsigned)
-        instructions.insert("sltiu", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0010011,
-            funct3: Some(0b011),
-            funct7: None
-        });
-
-        // I-type Instructions (Load-related)
-
-        // Load Byte
-        instructions.insert("lb", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0000011,
-            funct3: Some(0b000),
-            funct7: None
-        });
-
-        // Load Half
-        instructions.insert("lh", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0000011,
-            funct3: Some(0b001),
-            funct7: None
-        });
-
-        // Load Word
-        instructions.insert("lw", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0000011,
-            funct3: Some(0b010),
-            funct7: None
-        });
-
-        // Load Byte (Unsigned)
-        instructions.insert("lbu", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0000011,
-            funct3: Some(0b100),
-            funct7: None
-        });
-
-        // Load Half (Unsigned)
-        instructions.insert("lhu", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b0000011,
-            funct3: Some(0b101),
-            funct7: None
-        });
-
-        // S-type Instructions
-
-        // Store Byte
-        instructions.insert("sb", InstructionFormat {
-            fmt: InstructionType::S,
-            opcode: 0b0100011,
-            funct3: Some(0b000),
-            funct7: None
-        });
-
-        // Store Half
-        instructions.insert("sh", InstructionFormat {
-            fmt: InstructionType::S,
-            opcode: 0b0100011,
-            funct3: Some(0b001),
-            funct7: None
-        });
-
-        // Store Word
-        instructions.insert("sw", InstructionFormat {
-            fmt: InstructionType::S,
-            opcode: 0b0100011,
-            funct3: Some(0b010),
-            funct7: None
-        });
-
-        // B-type Instructions
-
-        // Branch ==
-        instructions.insert("beq", InstructionFormat {
-            fmt: InstructionType::B,
-            opcode: 0b1100011,
-            funct3: Some(0b000),
-            funct7: None
-        });
-
-        // Branch !=
-        instructions.insert("bne", InstructionFormat {
-            fmt: InstructionType::B,
-            opcode: 0b1100011,
-            funct3: Some(0b001),
-            funct7: None
-        });
-
-        // Branch <
-        instructions.insert("blt", InstructionFormat {
-            fmt: InstructionType::B,
-            opcode: 0b1100011,
-            funct3: Some(0b100),
-            funct7: None
-        });
-
-        // Branch >=
-        instructions.insert("bge", InstructionFormat {
-            fmt: InstructionType::B,
-            opcode: 0b1100011,
-            funct3: Some(0b101),
-            funct7: None
-        });
-
-        // Branch < (Unsigned)
-        instructions.insert("bltu", InstructionFormat {
-            fmt: InstructionType::B,
-            opcode: 0b1100011,
-            funct3: Some(0b110),
-            funct7: None
-        });
-
-        // Branch >= (Unsigned)
-        instructions.insert("bgeu", InstructionFormat {
-            fmt: InstructionType::B,
-            opcode: 0b1100011,
-            funct3: Some(0b111),
-            funct7: None
-        });
-
-        // Jump Instructions
-
-        // Jump and Link
-        instructions.insert("jal", InstructionFormat {
-            fmt: InstructionType::J,
-            opcode: 0b1101111,
-            funct3: None,
-            funct7: None
-        });
-
-        // Jump and Link Reg
-        instructions.insert("jalr", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b1100111,
-            funct3: None,
-            funct7: None
-        });
-
-        // U-type Instructions
-
-        // Load Upper Immediate
-        instructions.insert("lui", InstructionFormat {
-            fmt: InstructionType::U,
-            opcode: 0b0110111,
-            funct3: None,
-            funct7: None
-        });
-
-        // Add Upper Immediate to PC
-        instructions.insert("auipc", InstructionFormat {
-            fmt: InstructionType::U,
-            opcode: 0b0010111,
-            funct3: None,
-            funct7: None
-        });
-
-        // Environment Instructions
-
-        // Environment Call
-        instructions.insert("ecall", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b1110011,
-            funct3: Some(0b000),
-            funct7: Some(0b0000000)
-        });
-
-        // Environment Break
-        instructions.insert("ebreak", InstructionFormat {
-            fmt: InstructionType::I,
-            opcode: 0b1110011,
-            funct3: Some(0b000),
-            funct7: Some(0b0000001)
-        });
-
-        Self { instructions }
+        InstructionSet
     }
 
     pub fn get_instruction(&self, instr: &str) -> Option<&InstructionFormat> {
-        self.instructions.get(instr)
+        INSTRUCTIONS.get(instr)
     }
 }
