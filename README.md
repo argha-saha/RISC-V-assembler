7c95cb977ab7bef1fb69315b6648111cc3cef64b
argha-saha <saha56@purdue.edu>
2025-04-17 21:08:39 -0400

    Added the following RV64I instructions: addw, subw, sllw, srlw, sraw, addiw, slliw, srliw, sraiw, lwu, ld, and sd

--- README.md
+++ README.md
@@ -8,11 +8,17 @@ An assembler for the RISC-V instruction set, written in Rust.
 - Generates a binary file (`.bin`) containing machine code
 - Generates a hexdump file (`.hex`)
 
+## Instruction Support
+- RV32I: all r-type, i-type, s-type, b-type, u-type, and j-type (excluding atomics, fence, wfi, u/s/m ret)
+- RV64I: addw, subw, sllw, srlw, sraw, addiw, slliw, srliw, sraiw, lwu, ld, sd
+- Pseudo-instructions: nop, mv, not, neg, negw, sext.w, seqz, snez, sltz, sgtz, beqz, bnez, bltz, bgtz, bgt, ble, bgtu, bleu, j, jr, and ret
+
 ## Next Steps
 - Add support for .text and .data segments
-- Implement pseudo instructions (e.g. li, beqz, j)
+- Implement rest of the pseudo instructions
 - Implement RV32M multiply extension instructions
 - Implement RV32A atomic extension instructions
 
 ## References
-[RISC-V Reference](https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_CARD.pdf)
+[RISC-V Reference](https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_CARD.pdf)  
+[RISC-V ISA Pages](https://msyksphinz-self.github.io/riscv-isadoc/html/rv64i.html)  
--- src/assembler/instructions.rs
+++ src/assembler/instructions.rs
@@ -31,6 +31,14 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: Some(0b0000000)
     },
 
+    // ADD Word (RV64I)
+    "addw" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0111011,
+        funct3: Some(0b000),
+        funct7: Some(0b0000000)
+    },
+
     // SUB
     "sub" => InstructionFormat {
         fmt: InstructionType::R,
@@ -39,7 +47,7 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: Some(0b0100000)
     },
 
-    // SUB Word (RV64)
+    // SUB Word (RV64I)
     "subw" => InstructionFormat {
         fmt: InstructionType::R,
         opcode: 0b0111011,
@@ -79,6 +87,14 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: Some(0b0000000)
     },
 
+    // Shift Left Logical Word (RV64I)
+    "sllw" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0111011,
+        funct3: Some(0b001),
+        funct7: Some(0b0000000)
+    },
+
     // Shift Right Logical
     "srl" => InstructionFormat {
         fmt: InstructionType::R,
@@ -87,10 +103,26 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: Some(0b0000000)
     },
 
+    // Shift Right Logical Word (RV64I)
+    "srlw" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0111011,
+        funct3: Some(0b101),
+        funct7: Some(0b0000000)
+    },
+
     // Shift Right Arithmetic
     "sra" => InstructionFormat {
         fmt: InstructionType::R,
         opcode: 0b0110011,
+        funct3: Some(0b101),
+        funct7: Some(0b0100000)
+    },
+
+    // Shift Right Arithmetic Word (RV64I)
+    "sraw" => InstructionFormat {
+        fmt: InstructionType::R,
+        opcode: 0b0111011,
         funct3: Some(0b101),
         funct7: Some(0b0100000)
     },
@@ -121,7 +153,7 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: None
     },
 
-    // ADD Immediate Word (RV64)
+    // ADD Immediate Word (RV64I)
     "addiw" => InstructionFormat {
         fmt: InstructionType::I,
         opcode: 0b0011011,
@@ -161,6 +193,14 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: Some(0b0000000)
     },
 
+    // Shift Left Logical Immediate Word (RV64I)
+    "slliw" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0011011,
+        funct3: Some(0b001),
+        funct7: Some(0b0000000)
+    },
+
     // Shift Right Logical Immediate
     "srli" => InstructionFormat {
         fmt: InstructionType::I,
@@ -169,8 +209,24 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: Some(0b0000000)
     },
 
+    // Shift Right Logical Immediate Word (RV64I)
+    "srliw" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0011011,
+        funct3: Some(0b101),
+        funct7: Some(0b0000000)
+    },
+
     // Shift Right Arithmetic Immediate
     "srai" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0010011,
+        funct3: Some(0b101),
+        funct7: Some(0b0100000)
+    },
+
+    // Shift Right Arithmetic Immediate Word (RV64I)
+    "sraiw" => InstructionFormat {
         fmt: InstructionType::I,
         opcode: 0b0010011,
         funct3: Some(0b101),
@@ -219,6 +275,22 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         funct7: None
     },
 
+    // Load Word Unsigned (RV64I)
+    "lwu" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0000011,
+        funct3: Some(0b110),
+        funct7: None
+    },
+
+    // Load Doubleword (RV64I)
+    "ld" => InstructionFormat {
+        fmt: InstructionType::I,
+        opcode: 0b0000011,
+        funct3: Some(0b011),
+        funct7: None
+    },
+
     // Load Byte Unsigned
     "lbu" => InstructionFormat {
         fmt: InstructionType::I,
@@ -258,6 +330,14 @@ static INSTRUCTIONS: phf::Map<&'static str, InstructionFormat> = phf_map! {
         fmt: InstructionType::S,
         opcode: 0b0100011,
         funct3: Some(0b010),
+        funct7: None
+    },
+
+    // Store Doubleword (RV64I)
+    "sd" => InstructionFormat {
+        fmt: InstructionType::S,
+        opcode: 0b0100011,
+        funct3: Some(0b011),
         funct7: None
     },
 
